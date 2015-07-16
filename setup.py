160bfd5b3266e59d01f195e105733992837445e8
laugustogs <laugustogs@gmail.com>
2015-07-16 13:01:49 -0300

    version 0.1.150716

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'author': 'Leonardo dos Santos',
     'download_url': 'https://github.com/laugustogs/pwoogs',
     'author_email': 'leonardoags@usp.br',
-    'version': '0.1.150715',
+    'version': '0.1.150716',
     'install_requires': ['nose','numpy','scipy'],
     'packages': ['pwoogs'],
     'name': 'pwoogs'
