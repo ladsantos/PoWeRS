1f1be7c1d82ed22397cd65418c2710f2bd14efd0
laugustogs <laugustogs@gmail.com>
2015-07-22 10:13:42 -0300

    updated for version 0.1.150722

--- example/find_rotation.py
+++ example/find_rotation.py
@@ -4,6 +4,5 @@
 import numpy as np
 from pwoogs import estimate
 
-r = estimate.rotation(np.array([6026.5,6027.5]),3.47,26,x_wl=-0.0135,\
-    y_mult=0.996)
-r.find(np.array([0.2,-0.040]),np.array([0.7,-0.060]),gamma=2.0)
+r = estimate.rotation(np.array([6026.5,6027.5]),3.47,26,y_mult=0.996)
+r.find(np.array([0.2,-0.040,-0.0120]),np.array([0.7,-0.060,-0.0140]),gamma=2.0)
