73c915afeaf1528a02eb02767c45b6789e9820da
laugustogs <laugustogs@gmail.com>
2015-07-23 11:28:04 -0300

    updated for estimate.find()

--- example/find_rotation.py
+++ example/find_rotation.py
@@ -2,7 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from pwoogs import estimate
+from pwoogs import estimate,utils
 
-r = estimate.rotation(np.array([6026.5,6027.5]),3.47,26,y_mult=0.996)
-r.find(np.array([0.2,-0.040,-0.0120]),np.array([0.7,-0.060,-0.0140]),gamma=2.0)
+spec_window = np.array([6151.1,6152.1])
+#u = utils.arr_manage()
+#u.cut(spec_window[0]-10.,spec_window[1]+10.,'spectrum_full.dat','spectrum.dat')
+r = estimate.rotation(spec_window,3.47,26,y_mult=0.996)
+r.find(
+    np.array([0.0,-0.040,-0.0120]),
+    np.array([1.0,-0.060,-0.0140]),
+    gamma=10.0,
+    N=200,
+    alpha=0.2,
+    beta=0.1,
+    c_limit=1E-3
+    )
