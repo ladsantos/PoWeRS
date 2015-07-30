6d031c60b70661dbb83b0f25d33b5a086c342bb1
laugustogs <laugustogs@gmail.com>
2015-07-30 11:04:35 -0300

    new optionals, added user-defined gaussian

--- pwoogs/estimate.py
+++ pwoogs/estimate.py
@@ -26,7 +26,7 @@ class rotation(object):
     #  [ abund guess min, abund guess max ]]
     # v_macro is the macroturbulence velocity
     # Z is the atomic number of the chemical element that produces the line
-    def __init__(self,spec_window,v_macro,Z,**kwargs):
+    def __init__(self,spec_window,gauss,v_macro,Z,**kwargs):
         
         # Default optional parameters:
         
@@ -34,6 +34,11 @@ class rotation(object):
             self.vshift = kwargs['x_vel']
         else:
             self.vshift = 0.0
+            
+        if ('x_wl' in kwargs):
+            self.xshift = kwargs['x_wl']
+        else:
+            self.xshift = 0.0
         
         if ('y_add' in kwargs):
             self.yadd = kwargs['y_add']
@@ -49,11 +54,12 @@ class rotation(object):
         self.am = utils.arr_manage()
         self.n = normal.optimize()
         self.spec = spec_window
+        self.gauss = gauss
         self.v_m = v_macro
         self.Z = Z
         
     # Function that writes to params.txt
-    def write_params(self,log_rot_v,abund,wlshift):
+    def write_params(self,log_rot_v,abund):
         
         self.rot_v = 10**log_rot_v
         with open('params.txt','w') as f:
@@ -71,14 +77,14 @@ flux/int        0               None            See description on WRITEMOOG.ps
 damping         0               None            See description on WRITEMOOG.ps
 xshift          %.1f            %.4f            Shift on the x-axis, 1 = velocity, 2 = wavelength, for the observed spectrum
 yshift          %.4f            %.4f            Shift on the y-axis, 1 = additive, 2 = multiplicative, for the observed spectrum
-smoothing       0.052           0.0             1 = Gaussian, 2 = Lorentzian
+smoothing       %.3f            0.0             1 = Gaussian, 2 = Lorentzian
 darkening       0.6             None            Limb darkening coefficient of a rotational broadening function
 velocities      %.2f            %.3f            1 = macroturbulence, 2 = rotation
 nabunds         1               None            # of elements
 abund           %i              %.3f'''
                 % (self.spec[0],self.spec[0],self.spec[1],self.spec[1],
-                   self.vshift,wlshift,self.yadd,self.ymult,
-                   self.v_m,self.rot_v,
+                   self.vshift,self.xshift,self.yadd,self.ymult,
+                   self.gauss,self.v_m,self.rot_v,
                    self.Z,abund)
                 )
                 
@@ -93,13 +99,13 @@ abund           %i              %.3f'''
         
         # Applying corrections to the observed spectrum
         self.data = np.loadtxt('spectrum.dat')
-        self.data[:,0] = self.data[:,0] + p[2] - self.data[:,0]*(
+        self.data[:,0] = self.data[:,0] + self.xshift - self.data[:,0]*(
             self.c/(self.vshift*1E13+self.c)-1.0)
         self.data[:,1] = self.data[:,1]*self.ymult + self.yadd
         self.data_target = self.am.x_set_limits(self.spec[0],self.spec[1],self.data)
         
         # Running MOOGSILENT
-        self.write_params(p[0],p[1],p[2])
+        self.write_params(p[0],p[1])
         m = moog.run(silent=True)
         
         # Evaluating the performance
@@ -130,11 +136,21 @@ abund           %i              %.3f'''
             self.c_limit = kwargs['c_limit']
         else:
             self.c_limit = 1E-2
+            
+        if ('s_limit' in kwargs):
+            self.s_limit = kwargs['s_limit']
+        else:
+            self.s_limit = 1E-1
         
         if ('gamma' in kwargs):
             self.gamma = kwargs['gamma']
         else:
             self.gamma = 1.0
+            
+        if ('rho' in kwargs):
+            self.rho = kwargs['rho']
+        else:
+            self.rho = 0.1
         
         self.p_min = guess_min
         self.p_max = guess_max
@@ -144,16 +160,15 @@ abund           %i              %.3f'''
         # The estimation by CREPE is done in just one line:
         self.new_p_mean,self.new_p_sigma = self.n.estimate(self.perf,\
             self.p_mean,self.p_sigma,c_limit=self.c_limit,verbose=True,\
-                alpha=self.alpha,beta=self.beta,N=self.N)
+                alpha=self.alpha,beta=self.beta,N=self.N,rho=self.rho)
 
         # Printing and plotting the results
         print 'log_v_rot = %.3f p/m %.3f' % (self.new_p_mean[0],\
             self.new_p_sigma[0])
-        print 'wlshift = %.4f p/m %.4f' % (self.new_p_mean[2],\
-            self.new_p_sigma[2])
+        #print 'xshift = %.4f p/m %.4f' % (self.new_p_mean[2],\
+        #    self.new_p_sigma[2])
         print 'abund = %.3f p/m %.3f' % (self.new_p_mean[1],\
             self.new_p_sigma[1])
 
-        self.write_params(self.new_p_mean[0],self.new_p_mean[1],\
-            self.new_p_mean[2])
+        self.write_params(self.new_p_mean[0],self.new_p_mean[1])
         m = moog.run()
