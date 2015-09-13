9a1cca1a0b22dda1df8479c9d8ef4d9941f18952
RogueAstro <leonardoags@usp.br>
2015-09-12 23:24:19 -0300

    PEP0008 compliance and addition of some assertions

--- pwoogs/utils.py
+++ pwoogs/utils.py
@@ -2,31 +2,43 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 class arr_manage(object):
     
-    # This routine finds the index of a specific value (target) in a numpy-array
-    def find_index(self,target,arr):
+    # This routine finds the index of a value closest to the target in a 
+    # numpy-array
+    def find_index(self,target,array):
         
-        self.diff = np.abs(arr-target)
+        self.diff = np.abs(array-target)
         self.index = np.where(self.diff == min(self.diff))[0][0]
         return self.index
     
-    # This routine performs a "soft-pop" in a numpy array
+    # This routine returns a section of an array given the start and end values
     def x_set_limits(self,start,end,data2d):
         
+        assert start > data2d[0], 'Invalid start value'
+        assert end < data2d[-1], 'Invalid end value'
         self.start_index = self.find_index(start,data2d[:,0])
         self.end_index = self.find_index(end,data2d[:,0])
         return data2d[self.start_index:self.end_index]
     
-    # This routine performs a "hard-pop" from a data file
-    def cut(self,start,end,file_full,file_target):
+    # This routine writes the section of a datafile to a new one, for which 
+    # the start and end values are chosen for a specific column of the 
+    # datafile. By default, the column is 0
+    def cut(self,start,end,file_full,file_target,**kwargs):
         
+        # Setting default column to look for the start and end
+        if ('col' in kwargs):
+            self.col = kwargs['col']
+        else:
+            self.col = 0
+        
+        # ISSUE: the code works only with 2-column datafiles as of now
         self.data = np.loadtxt(file_full)
-        self.start_index = self.find_index(start,self.data[:,0])
-        self.end_index = self.find_index(end,self.data[:,0])
+        self.start_index = self.find_index(start,self.data[:,self.col])
+        self.end_index = self.find_index(end,self.data[:,self.col])
         self.N = self.end_index-self.start_index
+        self.M = len(self.data[0,:])
         with open(file_target,'w') as f:
             f.truncate()
             for i in range(self.N):
@@ -48,8 +60,8 @@ class arr_manage(object):
         return -p[1]/2./p[0]
     
     # This routine finds the multiplicative factor to correct the normalization
-    # of a region of the spectrum, using the highest region (radius) of the spectrum 
-    # insde a section contained on data
+    # of a region of the spectrum, using the highest region (radius) of the 
+    # spectrum inside a section contained on data
     def find_corr(self,data,radius,**kwargs):
         
         # Do you want the program to be silent?
@@ -62,12 +74,14 @@ class arr_manage(object):
         self.ind = self.find_index(self.m,data[:,1])
         self.stdev = np.std(data[self.ind-radius:self.ind+radius+1,1])
         if self.silent == False:
-            print 'Standard deviation of the spectrum on the selected data points = %.4f' % self.stdev
+            print 'Standard deviation of the spectrum on the ' \
+                  'selected data points = %.4f' % self.stdev
         return np.mean(data[self.ind-radius:self.ind+radius+1,1])
     
     # This routine finds the multiplicative factor to correct the normalization
-    # of a region of the spectrum, using an ensemble of user defined points, based on which
-    # the correction will be estimated around a region within a radius of points
+    # of a region of the spectrum, using an ensemble of user defined points, 
+    # based on which the correction will be estimated around a region within
+    # a radius of points
     def find_corr_from_ensemble(self,data,target_wls,radius):
         
         self.corrs = np.empty(len(target_wls),float)
